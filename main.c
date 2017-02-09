6e6b2bbf5d70eb5d099b04c2bffc74fcceb4f4aa
liliabek <liliabek@yahoo.fr>
2017-02-09 14:40:57 +0100

    modification des données new

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
 Néanmoins,
 pour assurer ces développements internationaux,
 défricher ces pays émergents, bâtir des
-marques de toutes pièces, il faut le
-financement : d’où l’importance des marchés
+marques de hhhhhhhhhh l’importance des marchés
 domestiques comme source de profit. Or ces
 marchés-là sont matures, comme en panne
-dans de nombreuses catégories, en particulier
-les produits dits de grande consommation
+dans de nombreuses 

319187e30f9f0ac7061c3532fd47575123d47c9e
liliabek <liliabek@yahoo.fr>
2017-02-09 14:35:50 +0100

    modification des données

---
+++ main.c
@@ -0,0 +1,9 @@
+Néanmoins,
+pour assurer ces développements internationaux,
+défricher ces pays émergents, bâtir des
+marques de toutes pièces, il faut le
+financement : d’où l’importance des marchés
+domestiques comme source de profit. Or ces
+marchés-là sont matures, comme en panne
+dans de nombreuses catégories, en particulier
+les produits dits de grande consommation
